84a7c48e84e1c2cc89d2df694dbfc2564eac93cd
davidenox <nocedav31@gmail.com>
2023-11-07 12:28:41 +0100

    Adding SelectionSort in Python

---
+++ Selection Sort/selection_sort.py
@@ -0,0 +1,12 @@
+def SelectionSort(A, size):
+    for i in range(size):
+        min = i
+        for j in range(i+1, size):
+            if A[j] < A[min]:
+                min = j
+        (A[i],A[min]) = (A[min],A[i])
+
+seq = [7,2,4,5,3,1]
+len = len(seq)
+SelectionSort(seq, len)
+print(seq)
