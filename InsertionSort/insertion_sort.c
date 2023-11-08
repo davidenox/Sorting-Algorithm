e634488f9e1e8a248547f56bb5d2f2f3e48a712d
davidenox <nocedav31@gmail.com>
2023-11-08 10:04:16 +0100

    Update

--- InsertionSort/insertion_sort.c
+++ InsertionSort/insertion_sort.c
@@ -2,9 +2,36 @@
 #include <stdlib.h>
 
 void InsertionSort( int arr[], int n){
-    int i, j, key;
+    int i, j, stored;
     for (i = 1; i<n ; ++i) {
-        key = arr[i];
-        
+        stored = arr[i];
+        j = i - 1;
+        while (j >=0 && arr[j] > stored ) {
+            arr[j+1] = arr[j];
+            j-=1;
+        }
+        arr[j + 1 ] = stored;
     }
 }
+
+/* Sample for output:
+
+void printArray(int arr[], int n)
+{
+    int i;
+    for (i=0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+ 
+int main()
+{
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    InsertionSort(arr, n);
+    printArray(arr, n);
+    return 0;
+}
+
+Time Complexity : O(n^2)
+*/
